046504d77463abdc77dd72ea8416d7d364f4e557
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-06-10 13:23:39 -0700

    successfully call api onto review page

---
+++ app/services/review_service.rb
@@ -0,0 +1,7 @@
+class ReviewService
+
+  def self.get_review
+    response = HTTParty.get("http://localhost:3000/reviews", headers: { "Authorization" => "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyLCJleHAiOjE1OTIxNzE4ODF9.wRlKY6eYWb-hy8La7aVh76uPoqezWh4B66hCwFcI7o4" })
+  end
+
+end
